9947ae5227dae8b1aaf50fa1e766e8945ef90a4d
mingyo <alsry922@gmail.com>
2022-10-05 23:26:46 +0900

    dfs 문제1 인접행렬

--- src/DFS/그래프탐색/Exercise1.java
+++ src/DFS/그래프탐색/Exercise1.java
@@ -1,16 +1,63 @@
 package DFS.그래프탐색;
 
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.StringTokenizer;
+//
+//public class Exercise1 {
+//  static final int MAX_N = 1000;
+//  static int n; //    정점 수
+//  static int m; //    간선 수
+//  static ArrayList<Integer>[] graph = new ArrayList[MAX_N + 1];    // 인접리스트
+//  static boolean[] visited = new boolean[MAX_N + 1];    // 방문 정보
+//  static int answer = 0;    // 도달 가능 정점의 갯수
+//
+//  public static void main(String[] args) throws IOException {
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    StringTokenizer st = new StringTokenizer(br.readLine());
+//    n = Integer.parseInt(st.nextToken());
+//    m = Integer.parseInt(st.nextToken());
+//    /*인접 리스트 초기화*/
+//    for (int i = 1; i <= n; i++) {
+//      graph[i] = new ArrayList<>();
+//    }
+//    for (int i = 0; i < m; i++) {
+//      st = new StringTokenizer(br.readLine());
+//      int start = Integer.parseInt(st.nextToken());
+//      int end = Integer.parseInt(st.nextToken());
+//      graph[start].add(end);
+//      graph[end].add(start);
+//    }
+//    /*1번 정점부터 dfs 시작*/
+//    visited[1] = true;
+//    dfs(1);
+//    System.out.println(answer);
+//  }
+//
+//  public static void dfs(int curVtx) {
+//    /*방문한 곳은 방문하지 않는다.*/
+//    for (int i = 0; i < graph[curVtx].size(); i++) {
+//      int nextVtx = graph[curVtx].get(i);
+//      if (!visited[nextVtx]) {
+//        visited[nextVtx] = true;
+//        answer++;
+//        dfs(nextVtx);
+//      }
+//    }
+//  }
+//}
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Exercise1 {
   static final int MAX_N = 1000;
   static int n; //    정점 수
   static int m; //    간선 수
-  static ArrayList<Integer>[] graph = new ArrayList[MAX_N + 1];    // 인접리스트
+  static int[][] graph = new int[MAX_N + 1][MAX_N+1];    // 인접 행렬
   static boolean[] visited = new boolean[MAX_N + 1];    // 방문 정보
   static int answer = 0;    // 도달 가능 정점의 갯수
 
@@ -19,31 +66,27 @@ public class Exercise1 {
     StringTokenizer st = new StringTokenizer(br.readLine());
     n = Integer.parseInt(st.nextToken());
     m = Integer.parseInt(st.nextToken());
-    /*인접 리스트 초기화*/
-    for (int i = 1; i <= n; i++) {
-      graph[i] = new ArrayList<>();
-    }
+    /*인접 행렬 초기화*/
     for (int i = 0; i < m; i++) {
       st = new StringTokenizer(br.readLine());
       int start = Integer.parseInt(st.nextToken());
       int end = Integer.parseInt(st.nextToken());
-      graph[start].add(end);
-      graph[end].add(start);
+      graph[start][end] = 1;
+      graph[end][start] = 1;
     }
-    /*1번 정점부터 bfs 시작*/
+    /*1번 정점부터 dfs 시작*/
     visited[1] = true;
-    bfs(1);
+    dfs(1);
     System.out.println(answer);
   }
 
-  public static void bfs(int curVtx) {
+  public static void dfs(int curVtx) {
     /*방문한 곳은 방문하지 않는다.*/
-    for (int i = 0; i < graph[curVtx].size(); i++) {
-      int nextVtx = graph[curVtx].get(i);
-      if (!visited[nextVtx]) {
+    for (int nextVtx = 1; nextVtx <= n; nextVtx++) {
+      if (graph[curVtx][nextVtx] == 1 && !visited[nextVtx]) {
         visited[nextVtx] = true;
         answer++;
-        bfs(nextVtx);
+        dfs(nextVtx);
       }
     }
   }
