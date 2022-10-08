33e73d885b847462b5bb8bb3c5d4b39d63eda301
mingyo <alsry922@gmail.com>
2022-10-08 14:50:46 +0900

    BaekJoon 10866

---
+++ src/BaekJoon/Baek10866.java
@@ -0,0 +1,64 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Baek10866 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder answer = new StringBuilder();
+    StringTokenizer st;
+    Deque<Integer> deque = new LinkedList<>();    // 수를 담을 덱
+    int N = Integer.parseInt(br.readLine());    // 입력 횟수
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      /*명령어 별로 처리*/
+      while (st.hasMoreTokens()) {
+        String command = st.nextToken();
+        if (command.startsWith("push_front")) {
+          deque.addFirst(Integer.parseInt(st.nextToken()));
+        } else if (command.startsWith("push_back")) {
+          deque.addLast(Integer.parseInt(st.nextToken()));
+        } else if (command.startsWith("pop_front")) {
+          if (deque.isEmpty()) {
+            answer.append(-1).append("\n");
+          } else {
+            answer.append(deque.removeFirst()).append("\n");
+          }
+        } else if (command.startsWith("pop_back")) {
+          if (deque.isEmpty()) {
+            answer.append(-1).append("\n");
+          } else {
+            answer.append(deque.removeLast()).append("\n");
+          }
+        } else if (command.startsWith("size")) {
+          answer.append(deque.size()).append("\n");
+        } else if (command.startsWith("empty")) {
+          if (deque.isEmpty()) {
+            answer.append(1).append("\n");
+          } else {
+            answer.append(0).append("\n");
+          }
+        } else if (command.startsWith("front")) {
+          if (deque.isEmpty()) {
+            answer.append(-1).append("\n");
+          } else {
+            answer.append(deque.getFirst()).append("\n");
+          }
+        } else if (command.startsWith("back")) {
+          if (deque.isEmpty()) {
+            answer.append(-1).append("\n");
+          } else {
+            answer.append(deque.getLast()).append("\n");
+          }
+        }
+      }
+    }
+    System.out.println(answer);
+  }
+}
