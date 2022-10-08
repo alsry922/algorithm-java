ce52a71f5ee2d9110e57be97af38f5dea4640841
mingyo <alsry922@gmail.com>
2022-10-08 13:50:08 +0900

    BaekJoon 10828

---
+++ src/BaekJoon/Baek10828.java
@@ -0,0 +1,55 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Baek10828 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder answer = new StringBuilder();
+    StringTokenizer st;
+    Stack<Integer> stack = new Stack<>();
+    int N = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      /*명령어 별로 처리*/
+      while (st.hasMoreTokens()) {
+        String command = st.nextToken();
+        if (command.startsWith("push")) {
+          stack.push(Integer.parseInt(st.nextToken()));
+        }
+        else if (command.startsWith("pop")) {
+          if (stack.empty()) {
+            answer.append(-1).append("\n");
+          }
+          else {
+            answer.append(stack.pop()).append("\n");
+          }
+        }
+        else if (command.startsWith("size")) {
+          answer.append(stack.size()).append("\n");
+        }
+        else if(command.startsWith("empty")) {
+          if (stack.empty()) {
+            answer.append(1).append("\n");
+          }
+          else {
+            answer.append(0).append("\n");
+          }
+        }
+        else if (command.startsWith("top")) {
+          if (stack.empty()) {
+            answer.append(-1).append("\n");
+          } else {
+            answer.append(stack.peek()).append("\n");
+          }
+        }
+      }
+    }
+    System.out.println(answer);
+  }
+}
