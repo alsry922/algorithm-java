73348ec3cdab204fd34e5f8840ec0a47e2130a26
mingyo <alsry922@gmail.com>
2022-10-11 18:51:44 +0900

    DP1 / subproblem을 그대로 합치면 되는 DP / 계단 오르기

---
+++ src/DP1/subproblem을_그대로_합치면_되는_DP/Exercise2.java
@@ -0,0 +1,25 @@
+package DP1.subproblem을_그대로_합치면_되는_DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Exercise2 {
+  static final int MAX_N = 1000;    // 최대 계단 높이
+  static int n;
+  static int[] dp = new int[MAX_N+1];
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    n = Integer.parseInt(br.readLine());
+    // dp[i] = 0번 층에서 i층까지 도달알 수 있는 서로 다른 가짓 수
+    dp[0] = 1;
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+    for (int i = 4; i <= n; i++) {
+      dp[i] = (dp[i-2] + dp[i-3]) % 10007;
+    }
+    System.out.println(dp[n]);
+  }
+}
