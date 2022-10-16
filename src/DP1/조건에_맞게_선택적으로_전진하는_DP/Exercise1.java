77e50d60994df99730e8e096afb52c65710cecb8
mingyo <alsry922@gmail.com>
2022-10-16 18:53:26 +0900

    DP1 / 조건에 맞게 선택적으로 전진하는 DP / 최대 증가 부분 수열 다른 풀이

--- src/DP1/조건에_맞게_선택적으로_전진하는_DP/Exercise1.java
+++ src/DP1/조건에_맞게_선택적으로_전진하는_DP/Exercise1.java
@@ -1,50 +1,48 @@
 package DP1.조건에_맞게_선택적으로_전진하는_DP;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Exercise1 {
-  static int MAX_VALUE = 10000;    // 나올 수 있는 수의 최대 숫자
   static int n;
-  static int[] seq;
-  static int[][] dp;
-  public static void dpInit() {
-    for (int i = 0; i <= n; i++) {
-      for (int j = 0; j <= MAX_VALUE; j++) {
-        dp[i][j] = Integer.MIN_VALUE;
-      }
+  static int maxNum;
+  static int[] nums;
+  static int[] dp;
+  static public void initialize() {
+    for (int i = 1; i <= maxNum; i++) {
+      dp[i] = Integer.MIN_VALUE;
     }
-    dp[0][0] = 0;
+    nums[0] = 0;
+    dp[0] = 0;
   }
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     n = Integer.parseInt(br.readLine());
-    seq = new int[n+1];
-    dp = new int[n+1][MAX_VALUE+1];
+    nums = new int[n + 1];
     StringTokenizer st = new StringTokenizer(br.readLine());
     for (int i = 1; i <= n; i++) {
-      seq[i] = Integer.parseInt(st.nextToken());
+      nums[i] = Integer.parseInt(st.nextToken());
     }
-    dpInit();
-    for (int i = 1; i <= n; i++) {
-      for (int j = 0; j <= MAX_VALUE; j++) {
-        if (j != seq[i]) {
-          dp[i][j] = dp[i-1][j];
-        }
-        else {
-          dp[i][j] = dp[i-1][j];
 
-          for (int k = 0; k < seq[i]; k++) {
-            if (dp[i-1][k] != Integer.MIN_VALUE) {
-              dp[i][j] = Math.max(dp[i-1][k]+1, dp[i][j]);
-            }
-          }
+    int[] temp = Arrays.copyOf(nums, nums.length);
+    Arrays.sort(temp);
+    maxNum = temp[temp.length-1];
+    dp = new int[maxNum+1];
+
+    initialize();
+    for (int i = 1; i <= n; i++) {
+      int j = nums[i];
+      for (int k = 0; k < j; k++) {
+        if (dp[k] != Integer.MIN_VALUE) {
+          dp[j] = Math.max(dp[k]+1, dp[j]);
         }
       }
     }
     int answer = 0;
-    for (int j = 0; j <= MAX_VALUE; j++) {
-      answer = Math.max(answer, dp[n][j]);
+    for (int i = 0; i <= maxNum; i++) {
+      answer = Math.max(answer, dp[i]);
     }
     System.out.println(answer);
   }
