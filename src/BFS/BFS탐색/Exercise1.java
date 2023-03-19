2b1a75bd9fd522eeccd1250e20ab666ecb0d35fc
alsry922 <alsry922@gmail.com>
2023-03-19 17:37:31 +0900

    BFS / BFS 탐색 / 네 방향 탈출 가능 여부 판별하기

--- src/BFS/BFS탐색/Exercise1.java
+++ src/BFS/BFS탐색/Exercise1.java
@@ -8,63 +8,64 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Pair {
-  int x;
-  int y;
+    int x, y;
 
-  Pair(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 public class Exercise1 {
-  static int m, n;    // 행, 열
-  static int grid[][];    // 격자
-  static boolean visited[][];    // 방문 체크
-  static Queue<Pair> queue = new LinkedList<>();    // 방문한 노드를 담아두는 큐
+    public static int N, M; //행, 열
+    public static int[][] grid;
+    public static boolean[][] visited;
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
+    public static Queue<Pair> queue = new LinkedList<>();
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(bf.readLine());
-    m = Integer.parseInt(st.nextToken());
-    n = Integer.parseInt(st.nextToken());
-    grid = new int[m][n];
-    visited = new boolean[m][n];
-    /*격자 초기화*/
-    for (int i = 0; i < m; i++) {
-      st = new StringTokenizer(bf.readLine());
-      for (int j = 0; j < n; j++) {
-        grid[i][j] = Integer.parseInt(st.nextToken());
-      }
-    }
-    visited[0][0] = true;
-    queue.add(new Pair(0, 0));
-    bfs();
-    System.out.println(visited[m - 1][n - 1] ? 1 : 0);
-  }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-  public static void bfs() {
-    int[] dx = new int[]{1, -1, 0, 0};
-    int[] dy = new int[]{0, 0, -1, 1};
-    while (!queue.isEmpty()) {
-      Pair curV = queue.poll();   // 현재 정점
-      /*인접한 모든 정점 확인*/
-      for (int i = 0; i < 4; i++) {
-        int nextX = curV.x + dx[i];
-        int nextY = curV.y + dy[i];
-        if (canGo(nextX, nextY)) {
-          visited[nextX][nextY] = true;
-          queue.add(new Pair(nextX, nextY));
+        grid = new int[N + 1][M + 1];
+        visited = new boolean[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-      }
+
+        visited[1][1] = true;
+        queue.add(new Pair(1, 1));
+        bfs(1, 1);
+        System.out.println(visited[N][M] ? 1 : 0);
     }
-  }
 
-  public static boolean canGo(int nextX, int nextY) {
-    return isRange(nextX, nextY) && !visited[nextX][nextY] && grid[nextX][nextY] == 1;
-  }
+    private static void bfs(int x, int y) {
+        while (!queue.isEmpty()) {
+            Pair currV = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nextX = currV.x + dx[i];
+                int nextY = currV.y + dy[i];
+                if (canGo(nextX, nextY)) {
+                    visited[nextX][nextY] = true;
+                    queue.add(new Pair(nextX, nextY));
+                }
+            }
+        }
 
-  public static boolean isRange(int x, int y) {
-    return 0 <= x && x < m && 0 <= y && y < n;
-  }
+    }
+
+    private static boolean canGo(int x, int y) {
+        return isInRange(x, y) && grid[x][y] != 0 && !visited[x][y];
+    }
+
+    private static boolean isInRange(int x, int y) {
+        return x >= 1 && x <= N && y >= 1 && y <= M;
+    }
 }
