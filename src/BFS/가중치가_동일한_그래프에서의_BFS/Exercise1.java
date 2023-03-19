4d55d5d23556acea80a99851f7a955ea1a9b9d75
alsry922 <alsry922@gmail.com>
2023-03-19 19:49:34 +0900

    BFS / 가중치가 동일한 그래프에서의 BFS / 최소 경로로 탈출하기

--- src/BFS/가중치가_동일한_그래프에서의_BFS/Exercise1.java
+++ src/BFS/가중치가_동일한_그래프에서의_BFS/Exercise1.java
@@ -1,67 +1,75 @@
 package BFS.가중치가_동일한_그래프에서의_BFS;
+// 최소 경로로 탈출하기
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Vertex1 {
-  int x;
-  int y;
+class Pair {
+    int x, y;
 
-  public Vertex1(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 public class Exercise1 {
-  static final int MAX_N = 100, MAX_M = 100;
-  static int n, m;    // 격자 크기
-  static int[][] grid = new int[MAX_N][MAX_M];    // 격자
-  static boolean[][] visited = new boolean[MAX_N][MAX_M];    // 방문 정점
-  static int[][] step = new int[MAX_N][MAX_M];    // 특정 시작 정점으로부터 각 정점까지의 최단거리
-  static Queue<Vertex1> queue = new LinkedList<>();    // bfs에 사용할 큐
+    public static int N, M; //행 열
+    public static int[][] grid;
+    public static boolean[][] visited;
+    public static int[][] step;
+    public static Queue<Pair> queue = new LinkedList<>();
 
-  public static boolean isInRange(int x, int y) {
-    return 0 <= x && x < n && 0 <= y && y < m;
-  }
-  public static boolean canGo(int x, int y) {
-    return isInRange(x, y) && !visited[x][y] && grid[x][y] == 1;
-  }
-  /*갈 수 있는 정점을 찾아 방문 표시하고 그 정점의 step배열에 +1을 해주는 함수*/
-  public static void bfs() {
-    int[] dx = new int[]{-1, 1, 0, 0};
-    int[] dy = new int[]{0, 0, -1, 1};
-    while(!queue.isEmpty()) {
-      Vertex1 currVtx = queue.poll();
-      for (int i = 0; i < 4; i++) {
-        int nextX = currVtx.x + dx[i];
-        int nextY = currVtx.y + dy[i];
-        if (canGo(nextX, nextY)) {
-          visited[nextX][nextY] = true;
-          step[nextX][nextY] = step[currVtx.x][currVtx.y]+1;
-          queue.add(new Vertex1(nextX,nextY));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        grid = new int[N + 1][M + 1];
+        visited = new boolean[N + 1][M + 1];
+        step = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-      }
-    }
-  }
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    n = Integer.parseInt(st.nextToken());
-    m = Integer.parseInt(st.nextToken());
-    /*grid 초기화*/
-    for (int i = 0; i < n; i++) {
-      st = new StringTokenizer(br.readLine());
-      for (int j = 0; j < m; j++) {
-        grid[i][j] = Integer.parseInt(st.nextToken());
-      }
+        visited[1][1] = true;
+        queue.add(new Pair(1, 1));
+        bfs(1, 1);
+        System.out.println(visited[N][M] ? step[N][M] : -1);
     }
-    /*(0,0)정점에서부터 bfs 시작*/
-    visited[0][0] = true;
-    step[0][0] = 0;
-    queue.add(new Vertex1(0, 0));
-    bfs();
-    System.out.println(step[n-1][m-1] > 0 ? step[n-1][m-1] : -1);
-  }
+
+    private static void bfs(int x, int y) {
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        while (!queue.isEmpty()) {
+            Pair currV = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nextX = currV.x + dx[i];
+                int nextY = currV.y + dy[i];
+                if (canGo(nextX, nextY)) {
+                    visited[nextX][nextY] = true;
+                    queue.add(new Pair(nextX, nextY));
+                    step[nextX][nextY] = step[currV.x][currV.y]+1;
+                }
+            }
+        }
+    }
+
+    private static boolean canGo(int x, int y) {
+        return isInRange(x, y) && grid[x][y] == 1 && !visited[x][y];
+    }
+
+    private static boolean isInRange(int x, int y) {
+        return (x >= 1 && x <= N) && (y >= 1 && y <= M);
+    }
 }
