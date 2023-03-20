160f14d43873a0aefd0e31639c94431ab486b972
alsry922 <alsry922@gmail.com>
2023-03-20 19:32:31 +0900

    DP1 / 아이템을 적절히 고르는 문제 / 동전 거슬러주기

--- src/DP1/아이템을_적절히_고르는_문제/Exercise1.java
+++ src/DP1/아이템을_적절히_고르는_문제/Exercise1.java
@@ -1,4 +1,5 @@
 package DP1.아이템을_적절히_고르는_문제;
+// 동전 거슬러주기
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,41 +7,43 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Exercise1 {
-  static final int MAX_VALUE = 10001;
-  static int n; //동전 종류 갯수
-  static int m; //거스름돈 금액
-  static int[] dp; //동전의 합이 i일 때, 고른 동전의 최소 갯수
-  static int[] coins; //동전 종류
+    public static int N, M; //동전 종류, 금액
+    public static int[] coins;
+    public static int[] dp;
 
-  public static void initialize() {
-    for (int i = 0; i <= m; i++) {
-      dp[i] = MAX_VALUE;
-    }
-    dp[0] = 0;
-  }
+    public static int MAX_ANS = 10001;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    n = Integer.parseInt(st.nextToken());
-    m = Integer.parseInt(st.nextToken());
-    dp = new int[m + 1];
-    coins = new int[n];
+        coins = new int[N + 1];
+        dp = new int[M + 1];
 
-    st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < n; i++) {
-      coins[i] = Integer.parseInt(st.nextToken());
-    }
-
-    initialize();
-    for (int i = 1; i <= m; i++) {
-      for (int j = 0; j < n; j++) {
-        if (i >= coins[j]) {
-          dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            coins[i] = Integer.parseInt(st.nextToken());
         }
-      }
+
+        dpInit();
+
+        for (int i = 1; i <= M; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i >= coins[j] && dp[i-coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[M] == 10001 ? -1 : dp[M]);
     }
 
-    System.out.println(dp[m] != MAX_VALUE ? dp[m] : -1);
-  }
+    private static void dpInit() {
+        for (int i = 1; i <= M; i++) {
+            dp[i] = MAX_ANS;
+        }
+    }
+
+
 }
