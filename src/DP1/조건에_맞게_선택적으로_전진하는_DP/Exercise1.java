93a18ff74d4cb329d4386cbd0be63673aabeb180
alsry922 <alsry922@gmail.com>
2023-03-20 18:33:42 +0900

    DP1 / 조건에 맞게 선택적으로 전진하는 DP / 최대 증가 부분 수열

--- src/DP1/조건에_맞게_선택적으로_전진하는_DP/Exercise1.java
+++ src/DP1/조건에_맞게_선택적으로_전진하는_DP/Exercise1.java
@@ -1,49 +1,47 @@
 package DP1.조건에_맞게_선택적으로_전진하는_DP;
+//최대 증가 부분 수열
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Exercise1 {
-  static int n;
-  static int maxNum;
-  static int[] nums;
-  static int[] dp;
-  static public void initialize() {
-    for (int i = 1; i <= maxNum; i++) {
-      dp[i] = Integer.MIN_VALUE;
-    }
-    nums[0] = 0;
-    dp[0] = 0;
-  }
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    n = Integer.parseInt(br.readLine());
-    nums = new int[n + 1];
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    for (int i = 1; i <= n; i++) {
-      nums[i] = Integer.parseInt(st.nextToken());
-    }
+    public static int N; //수열 길이
+    public static int[] seq; // 수열
+    public static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        seq = new int[N + 1];
+        dp = new int[N + 1];
 
-    int[] temp = Arrays.copyOf(nums, nums.length);
-    Arrays.sort(temp);
-    maxNum = temp[temp.length-1];
-    dp = new int[maxNum+1];
-
-    initialize();
-    for (int i = 1; i <= n; i++) {
-      int j = nums[i];
-      for (int k = 0; k < j; k++) {
-        if (dp[k] != Integer.MIN_VALUE) {
-          dp[j] = Math.max(dp[k]+1, dp[j]);
+        initializeDP();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
         }
-      }
+
+        findMaxSubSequence();
+
+        System.out.println(Arrays.stream(dp).max().getAsInt());
     }
-    int answer = 0;
-    for (int i = 0; i <= maxNum; i++) {
-      answer = Math.max(answer, dp[i]);
+
+    private static void findMaxSubSequence() {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (seq[j] < seq[i]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
     }
-    System.out.println(answer);
-  }
+
+    private static void initializeDP() {
+        for (int i = 1; i <= N; i++) {
+            dp[i] = Integer.MIN_VALUE;
+        }
+        dp[1] = 1;
+    }
 }
