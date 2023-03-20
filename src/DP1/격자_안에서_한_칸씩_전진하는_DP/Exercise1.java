142927688e008d9439a04988ae3b4f2c26b4ea0f
alsry922 <alsry922@gmail.com>
2023-03-20 15:18:38 +0900

    DP1 / 격자 안에서 한 칸씩 전진하는 DP / 정수 사각형 최대 합

--- src/DP1/격자_안에서_한_칸씩_전진하는_DP/Exercise1.java
+++ src/DP1/격자_안에서_한_칸씩_전진하는_DP/Exercise1.java
@@ -1,43 +1,43 @@
 package DP1.격자_안에서_한_칸씩_전진하는_DP;
+//정수 사각형 최대 합
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Exercise1 {
-  static final int MAX_N = 100;    // 격자 최대 크기
-  static int n;
-//  static int[][] grid = new int[MAX_N+1][MAX_N+1];
-//  static int[][] dp = new int[MAX_N+1][MAX_N+1];
-  static int[][] grid;
-  static int[][] dp;
-  public static void dpInit() {
-    dp[1][1] = grid[1][1];
-    for (int i = 1; i <= n; i++) {
-      dp[1][i] = grid[1][i] + dp[1][i-1];
-      dp[i][1] = grid[i][1] + dp[i-1][1];
-    }
-  }
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    n = Integer.parseInt(br.readLine());
-    grid = new int[n+1][n+1];
-    dp = new int[n+1][n+1];
-    StringTokenizer st;
-    for (int i = 1; i <= n; i++) {
-      st = new StringTokenizer(br.readLine());
-      for (int j = 1; j <= n; j++) {
-        grid[i][j] = Integer.parseInt(st.nextToken());
-      }
-    }
-    dpInit();
+    public static int N; //격자 크기
+    public static int[][] grid; //격자
+    public static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        grid = new int[N + 1][N + 1];
+        dp = new int[N + 1][N + 1];
 
-    for (int i = 2; i <= n; i++) {
-      for (int j = 2; j <= n; j++) {
-        dp[i][j] = grid[i][j]+Math.max(dp[i-1][j], dp[i][j-1]);
-      }
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        initializeDP();
+        for (int i = 2; i <= N; i++) {
+            for (int j = 2; j <= N; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        System.out.println(dp[N][N]);
     }
-    System.out.println(dp[n][n]);
-  }
+
+    private static void initializeDP() {
+        dp[1][1] = grid[1][1];
+        for (int i = 1; i <= N; i++) {
+            dp[1][i] = dp[1][i - 1] + grid[1][i];
+            dp[i][1] = dp[i - 1][1] + grid[i][1];
+        }
+    }
 }
