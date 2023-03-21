1f5d0a5c7b39349e2ba583694b84977db64eb388
alsry922 <alsry922@gmail.com>
2023-03-21 13:09:52 +0900

    DP1 / 아이템을 적절히 고르는 문제 / 부분 수열의 합이 m

--- src/DP1/아이템을_적절히_고르는_문제/Exercise2.java
+++ src/DP1/아이템을_적절히_고르는_문제/Exercise2.java
@@ -1,4 +1,5 @@
 package DP1.아이템을_적절히_고르는_문제;
+//부분 수열의 합이 m
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,38 +7,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Exercise2 {
-  static int n; // 수열의 원소 갯수
-  static int m; // 부분 수열의 합
-  static int[] seq; // 수열
-  static int[] dp;
-  static final int MAX_VALUE = 10001;
+    public static int N, M; //수열 길이, 부분 수열 목표 합
+    public static int[] seq; //수열
+    public static int[] dp;
+    public static int MAX_ANS = 10001;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-  public static void initialize() {
-    for (int i = 0; i <= m; i++) {
-      dp[i] = MAX_VALUE;
-    }
-    dp[0] = 0;
-  }
+        seq = new int[N + 1];
+        dp = new int[M + 1];
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    n = Integer.parseInt(st.nextToken());
-    m = Integer.parseInt(st.nextToken());
-    seq = new int[n+1];
-    dp = new int[m+1];
-    st = new StringTokenizer(br.readLine());
-    for (int i = 1; i <= n; i++) {
-      seq[i] = Integer.parseInt(st.nextToken());
-    }
-    initialize();
-    for (int i = 1; i <= n; i++) {
-      for (int j = m; j >= 0; j--) {
-        if (j >= seq[i]) {
-          dp[j] = Math.min(dp[j], dp[j-seq[i]]+1);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
         }
-      }
+
+        dpInit();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = M; j >= 1; j--) {
+                if (j >= seq[i]) {
+                    dp[j] = Math.min(dp[j], dp[j - seq[i]] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[M] == MAX_ANS ? -1 : dp[M]);
     }
-    System.out.println(dp[m] != MAX_VALUE ? dp[m] : -1);
-  }
+
+    private static void dpInit() {
+        for (int i = 1; i <= M; i++) {
+            dp[i] = MAX_ANS;
+        }
+    }
 }
