1ee175658651bcd63686fe7407420b64eb17d237
alsry922 <alsry922@gmail.com>
2023-04-13 14:52:29 +0900

    DFS / DFS 탐색 / 그래프 탐색

--- src/INTERMEDIATE_LOW/DFS/그래프탐색/Exercise1.java
+++ src/INTERMEDIATE_LOW/DFS/그래프탐색/Exercise1.java
@@ -5,26 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-
+//그래프 탐색
 public class Exercise1 {
-  public static int N, M; //정점, 간선
+  public static int N, M; //정점 갯수, 간선 갯수
   public static ArrayList<Integer>[] graph;
   public static boolean[] visited;
-  public static int answer = 0;
+  public static int answer;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-
-    graph = new ArrayList[N+1];
+    graph = new ArrayList[N + 1];
     visited = new boolean[N + 1];
-
-    for (int i = 1; i <= N; i++) {
+    for (int i = 0; i <= N; i++) {
       graph[i] = new ArrayList<>();
     }
-    //그래프 채우기
-    for (int i = 0; i < M; i++) {
+
+    for (int i = 1; i <= M; i++) {
       st = new StringTokenizer(br.readLine());
       int start = Integer.parseInt(st.nextToken());
       int end = Integer.parseInt(st.nextToken());
@@ -34,7 +32,6 @@ public class Exercise1 {
 
     visited[1] = true;
     dfs(1);
-
     System.out.println(answer);
   }
 
