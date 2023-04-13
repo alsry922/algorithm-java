94470e3f4f359240437a2dce7969ae8e739c392c
alsry922 <alsry922@gmail.com>
2023-04-13 15:21:42 +0900

    DFS / DFS 탐색 / 두 방향 탈출 가능 여부 판별하기

--- src/INTERMEDIATE_LOW/DFS/그래프탐색/Exercise2.java
+++ src/INTERMEDIATE_LOW/DFS/그래프탐색/Exercise2.java
@@ -1,53 +1,64 @@
 package INTERMEDIATE_LOW.DFS.그래프탐색;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+//두 방향 탈출 가능 여부 판별하기
 public class Exercise2 {
-  public static int N, M; //행, 열
-  public static int[][] grid;
-  public static boolean[][] visited;
-  public static int[] dx = {1, 0};
-  public static int[] dy = {0, 1};
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
+    static class Pair {
+        int x, y;
 
-    grid = new int[N + 1][M + 1];
-    visited = new boolean[N + 1][M + 1];
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static int N, M; //행, 열
+    public static int[][] grid;
+    public static boolean[][] visited;
+    public static int[][] order;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        grid = new int[N + 1][M + 1];
+        visited = new boolean[N + 1][M + 1];
+        order = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-    for (int i = 1; i <= N; i++) {
-      st = new StringTokenizer(br.readLine());
-      for (int j = 1; j <= M; j++) {
-        grid[i][j] = Integer.parseInt(st.nextToken());
-      }
+        visited[1][1] = true;
+        dfs(1, 1);
+        System.out.println(visited[N][M] ? 1 : 0);
     }
 
-    visited[1][1] = true;
-    escape(1, 1);
-    System.out.println(visited[N][M] ? 1 : 0);
-  }
+    private static void dfs(int x, int y) {
+        int[] dx = {1, 0};
+        int[] dy = {0, 1};
 
-  private static void escape(int x, int y) {
-    for (int i = 0; i < 2; i++) {
-      int nextX = x + dx[i];
-      int nextY = y + dy[i];
-      if (canGo(nextX, nextY)) {
-        visited[nextX][nextY] = true;
-        escape(nextX, nextY);
-      }
+        for (int i = 0; i < 2; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (canGo(nx, ny)) {
+                visited[nx][ny] = true;
+                order[nx][ny] = order[x][y] + 1;
+                dfs(nx, ny);
+            }
+        }
     }
-  }
 
-  private static boolean canGo(int x, int y) {
-    return isInRange(x, y) && grid[x][y] != 0 && !visited[x][y];
-  }
+    private static boolean canGo(int x, int y) {
+        return isInRange(x, y) && !visited[x][y] && grid[x][y] == 1;
+    }
 
-  private static boolean isInRange(int x, int y) {
-    return x >= 1 && x <= N && y >= 1 && y <= M;
-  }
-
-
+    private static boolean isInRange(int x, int y) {
+        return x >= 1 && x <= N && y >= 1 && y <= M;
+    }
 }
