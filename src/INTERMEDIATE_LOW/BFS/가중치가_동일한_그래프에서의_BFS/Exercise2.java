d8dcb5cd1bf982d7cc639fb677bbbecca268df65
alsry922 <alsry922@gmail.com>
2023-04-17 20:59:37 +0900

    BFS / 가중치가 동일한 그래프에서의 BFS / 나이트

--- src/INTERMEDIATE_LOW/BFS/가중치가_동일한_그래프에서의_BFS/Exercise2.java
+++ src/INTERMEDIATE_LOW/BFS/가중치가_동일한_그래프에서의_BFS/Exercise2.java
@@ -1,5 +1,4 @@
 package INTERMEDIATE_LOW.BFS.가중치가_동일한_그래프에서의_BFS;
-// 나이트
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,74 +7,71 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Vertex {
-    int x, y;
-
-    public Vertex(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
+// 나이트
 public class Exercise2 {
-    public static int n; // grid size
-    public static boolean[][] visited;
-    public static int[] x = new int[2];
-    public static int[] y = new int[2];
-    public static int[][] step;
-    public static Queue<Vertex> bfsQ = new LinkedList<>();
+    static class Pair {
+        int x, y;
 
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static int N; //격자 크기
+    public static int[][] step;
+    public static boolean[][] visited;
+    public static int[] posX;
+    public static int[] posY;
+    public static Queue<Pair> bfsQ = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        visited = new boolean[n + 1][n + 1];
-        step = new int[n + 1][n + 1];
+        N = Integer.parseInt(br.readLine());
+
+        step = new int[N][N];
+        visited = new boolean[N][N];
+        posX = new int[2];
+        posY = new int[2];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 2; i++) {
-            x[i] = Integer.parseInt(st.nextToken());
-            y[i] = Integer.parseInt(st.nextToken());
-        }
+        posX[0] = Integer.parseInt(st.nextToken())-1;
+        posY[0] = Integer.parseInt(st.nextToken())-1;
+        posX[1] = Integer.parseInt(st.nextToken())-1;
+        posY[1] = Integer.parseInt(st.nextToken())-1;
 
-        Vertex startV = new Vertex(x[0], y[0]);
-        Vertex endV = new Vertex(x[1], y[1]);
+        visited[posX[0]][posY[0]] = true;
+        bfsQ.add(new Pair(posX[0], posY[0]));
 
-        visited[startV.x][startV.y] = true;
-        bfsQ.add(startV);
-        bfs();
-
-        System.out.println(visited[endV.x][endV.y] ? step[endV.x][endV.y] : -1);
-
-    }
-
-    public static void bfs() {
-        int[] dx = {-2, -2, -1, -1,  1, 1,  2, 2};
-        int[] dy = {-1,  1, -2,  2, -2, 2, -1, 1};
+        int[] dx = {2,  2,  1, 1, -1, -1, -2, -2};
+        int[] dy = {-1, 1, -2, 2, -2,  2, -1,  1};
 
         while (!bfsQ.isEmpty()) {
-            Vertex currV = bfsQ.poll();
-
-            int currX = currV.x;
-            int currY = currV.y;
-
+            Pair pair = bfsQ.poll();
+            int cx = pair.x;
+            int cy = pair.y;
             for (int i = 0; i < 8; i++) {
-                int nextX = currX + dx[i];
-                int nextY = currY + dy[i];
-                if (canGo(nextX, nextY)) {
-                    visited[nextX][nextY] = true;
-                    step[nextX][nextY] = step[currX][currY] + 1;
-                    bfsQ.add(new Vertex(nextX, nextY));
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                if (canGo(nx, ny)) {
+                    push(nx, ny, step[cx][cy] + 1);
                 }
             }
         }
 
+        System.out.println(visited[posX[1]][posY[1]] ? step[posX[1]][posY[1]] : -1);
+
     }
 
-    public static boolean canGo(int x, int y) {
-        return isInRange(x, y) && !visited[x][y];
+    private static void push(int nx, int ny, int dist) {
+        visited[nx][ny] = true;
+        step[nx][ny] = dist;
+        bfsQ.add(new Pair(nx, ny));
     }
 
-    private static boolean isInRange(int x, int y) {
-        return x >= 1 && x <= n && y >= 1 && y <= n;
+    private static boolean canGo(int nx, int ny) {
+        return isInRange(nx, ny) && !visited[nx][ny];
+    }
+
+    private static boolean isInRange(int nx, int ny) {
+        return nx >= 0 && nx < N && ny >= 0 && ny < N;
     }
 }
