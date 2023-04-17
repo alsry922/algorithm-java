bfdb8c112130c1a49ead15530a6cd0709d9893ed
alsry922 <alsry922@gmail.com>
2023-04-17 16:32:49 +0900

    BFS / 가중치가 동일한 그래프에서의 BFS / 최소 경로로 탈출하기

--- src/INTERMEDIATE_LOW/BFS/가중치가_동일한_그래프에서의_BFS/Exercise1.java
+++ src/INTERMEDIATE_LOW/BFS/가중치가_동일한_그래프에서의_BFS/Exercise1.java
@@ -1,5 +1,4 @@
 package INTERMEDIATE_LOW.BFS.가중치가_동일한_그래프에서의_BFS;
-// 최소 경로로 탈출하기
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,68 +7,63 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Pair {
-    int x, y;
-
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
+// 최소 경로로 탈출하기
 public class Exercise1 {
-    public static int N, M; //행 열
-    public static int[][] grid;
-    public static boolean[][] visited;
-    public static int[][] step;
-    public static Queue<Pair> queue = new LinkedList<>();
+    static class Pair {
+        int x, y;
 
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static int N, M; //행, 열
+    public static int[][] grid;
+    public static int[][] step;
+    public static boolean[][] visited;
+    public static Queue<Pair> bfsQ = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        grid = new int[N + 1][M + 1];
-        visited = new boolean[N + 1][M + 1];
-        step = new int[N + 1][M + 1];
-
-        for (int i = 1; i <= N; i++) {
+        grid = new int[N][M];
+        visited = new boolean[N][M];
+        step = new int[N][M];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= M; j++) {
+            for (int j = 0; j < M; j++) {
                 grid[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        visited[1][1] = true;
-        queue.add(new Pair(1, 1));
-        bfs(1, 1);
-        System.out.println(visited[N][M] ? step[N][M] : -1);
-    }
-
-    private static void bfs(int x, int y) {
+        visited[0][0] = true;
+        bfsQ.add(new Pair(0, 0));
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-
-        while (!queue.isEmpty()) {
-            Pair currV = queue.poll();
+        while (!bfsQ.isEmpty()) {
+            Pair curr = bfsQ.poll();
+            int cx = curr.x;
+            int cy = curr.y;
             for (int i = 0; i < 4; i++) {
-                int nextX = currV.x + dx[i];
-                int nextY = currV.y + dy[i];
-                if (canGo(nextX, nextY)) {
-                    visited[nextX][nextY] = true;
-                    queue.add(new Pair(nextX, nextY));
-                    step[nextX][nextY] = step[currV.x][currV.y]+1;
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                if (canGo(nx, ny)) {
+                    visited[nx][ny] = true;
+                    step[nx][ny] = step[cx][cy] + 1;
+                    bfsQ.add(new Pair(nx, ny));
                 }
             }
         }
+        System.out.println(visited[N-1][M-1] ? step[N-1][M-1] : -1);
     }
 
-    private static boolean canGo(int x, int y) {
-        return isInRange(x, y) && grid[x][y] == 1 && !visited[x][y];
+    private static boolean canGo(int nx, int ny) {
+        return isInRange(nx, ny) && !visited[nx][ny] && grid[nx][ny] == 1;
     }
 
-    private static boolean isInRange(int x, int y) {
-        return (x >= 1 && x <= N) && (y >= 1 && y <= M);
+    private static boolean isInRange(int nx, int ny) {
+        return nx >= 0 && nx < N && ny >= 0 && ny < M;
     }
 }
