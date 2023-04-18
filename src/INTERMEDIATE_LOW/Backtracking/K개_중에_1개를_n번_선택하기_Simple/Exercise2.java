2516b2c87de6b0a6363b3bc4781eb0f42447c337
alsry922 <alsry922@gmail.com>
2023-04-18 19:01:25 +0900

    backtracking / K개 중에 1개를 n번 선택하기(simple) / 아름다운 수

--- src/INTERMEDIATE_LOW/Backtracking/K개_중에_1개를_n번_선택하기_Simple/Exercise2.java
+++ src/INTERMEDIATE_LOW/Backtracking/K개_중에_1개를_n번_선택하기_Simple/Exercise2.java
@@ -1,27 +1,24 @@
 package INTERMEDIATE_LOW.Backtracking.K개_중에_1개를_n번_선택하기_Simple;
-//아름다운 수
-
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-
+//아름다운 수
 public class Exercise2 {
-    public static int n; //자릿 수
-    public static ArrayList<Integer> arr = new ArrayList<>();
-    public static int answer;
+    public static int N; //자릿 수
+    public static ArrayList<Integer> selectList = new ArrayList<>();
+    public static int answer = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        choose(1, n);
-
+        choose(0);
         System.out.println(answer);
     }
 
-    public static void choose(int currPos, int n) {
-        if (currPos > n) {
+    private static void choose(int cnt) {
+        if (cnt == N) {
             if (isBeautifulNum()) {
                 answer++;
             }
@@ -29,19 +26,19 @@ public class Exercise2 {
         }
 
         for (int i = 1; i <= 4; i++) {
-            arr.add(i);
-            choose(currPos + 1, n);
-            arr.remove(arr.size() - 1);
+            selectList.add(i);
+            choose(cnt + 1);
+            selectList.remove(selectList.size() - 1);
         }
     }
 
     private static boolean isBeautifulNum() {
-        for (int i = 0; i < n; i += arr.get(i)) {
-            if (i + arr.get(i) - 1 >= n) {
+        for (int i = 0; i < selectList.size(); i += selectList.get(i)) {
+            if (i + selectList.get(i) - 1 >= N) {
                 return false;
             }
-            for (int j = i; j < i + arr.get(i); j++) {
-                if (arr.get(j) != arr.get(i)) {
+            for (int j = i; j < i + selectList.get(i); j++) {
+                if (!selectList.get(i).equals(selectList.get(j))) {
                     return false;
                 }
             }
