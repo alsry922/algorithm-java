89f321b0baaef14d692b794ae15cc76d088dd92a
alsry922 <alsry922@gmail.com>
2023-04-23 00:58:23 +0900

    backtracking / N개 중에 M개 고르기 / xor 결과 최대 만들기

--- src/INTERMEDIATE_LOW/Backtracking/N개_중에_M개_고르기/Exercise2.java
+++ src/INTERMEDIATE_LOW/Backtracking/N개_중에_M개_고르기/Exercise2.java
@@ -1,5 +1,4 @@
 package INTERMEDIATE_LOW.Backtracking.N개_중에_M개_고르기;
-// xor 결과 최대 만들기
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,49 +6,49 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+// xor 결과 최대 만들기
 public class Exercise2 {
-    public static int n, m; // 입력 갯수, 뽑을 갯수
-    public static int answer = 0;
+    public static int N, M; //N개 중 M개 고르기
     public static int[] nums;
-    public static ArrayList<Integer> selected = new ArrayList<>();
+    public static ArrayList<Integer> select = new ArrayList<>();
+    public static int answer = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        nums = new int[n + 1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        choose(1, 0);
-
+        findMaxXor(0, 0);
         System.out.println(answer);
     }
 
-    private static void choose(int currPos, int cnt) {
-        if (currPos > n) {
-            if (cnt == m) {
-                answer = calculateAnswer();
+    private static void findMaxXor(int idx, int cnt) {
+        if (idx == N) {
+            if (cnt == M) {
+                answer = Math.max(answer, calc());
             }
             return;
         }
 
-        selected.add(nums[currPos]);
-        choose(currPos + 1, cnt + 1);
-        selected.remove(selected.size() - 1);
-        choose(currPos + 1, cnt);
+        select.add(nums[idx]);
+        findMaxXor(idx + 1, cnt + 1);
+        select.remove(select.size() - 1);
+        findMaxXor(idx + 1, cnt);
+
     }
 
-    private static int calculateAnswer() {
-        int result = 0;
-        for (int i = 0; i < selected.size(); i++) {
-            result ^= selected.get(i);
+    private static int calc() {
+        int result = select.get(0);
+        for (int i = 1; i < select.size(); i++) {
+            result = result ^ select.get(i);
         }
-        return Math.max(answer, result);
+        return result;
     }
 }
