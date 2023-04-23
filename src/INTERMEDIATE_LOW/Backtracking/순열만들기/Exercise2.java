f0e427608feb20fbb5fdb327bad997fe072225ab
alsry922 <alsry922@gmail.com>
2023-04-23 16:22:31 +0900

    backtracking / 순열만들기 / 거꾸로 순열

--- src/INTERMEDIATE_LOW/Backtracking/순열만들기/Exercise2.java
+++ src/INTERMEDIATE_LOW/Backtracking/순열만들기/Exercise2.java
@@ -1,46 +1,41 @@
 package INTERMEDIATE_LOW.Backtracking.순열만들기;
-// 거꾸로 순열
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+// 거꾸로 순열
 
 public class Exercise2 {
-    public static int n;
-    public static boolean[] visited;
+    public static int N; //가능한 수의 범위
+    public static boolean[] selected;
     public static ArrayList<Integer> permutation = new ArrayList<>();
-    public static StringBuffer sb = new StringBuffer();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        visited = new boolean[n + 1];
+        N = Integer.parseInt(br.readLine());
+        selected = new boolean[N + 1];
 
-        choose(0);
-        System.out.println(sb);
+        getPermutation(0);
     }
 
-    private static void choose(int currPos) {
-        if (currPos >= n) {
-            print();
+    private static void getPermutation(int cnt) {
+        if (cnt == N) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < permutation.size(); i++) {
+                sb.append(permutation.get(i)).append(" ");
+            }
+            System.out.println(sb);
+            return;
         }
 
-        for (int i = n; i >= 1; i--) {
-            if (!visited[i]) {
-                visited[i] = true;
+        for (int i = N; i >= 1; i--) {
+            if (!selected[i]) {
+                selected[i] = true;
                 permutation.add(i);
-                choose(currPos + 1);
-                visited[i] = false;
+                getPermutation(cnt + 1);
+                selected[i] = false;
                 permutation.remove(permutation.size() - 1);
             }
         }
-    }
-
-    private static void print() {
-        for (int i = 0; i < permutation.size(); i++) {
-            sb.append(permutation.get(i)).append(" ");
-        }
-        sb.append("\n");
     }
 }
