ba4afc767025beb8a4372dff6d4aa9bab58574a6
alsry922 <alsry922@gmail.com>
2023-04-30 21:54:27 +0900

    simulation / 격자 안에서 터지고 떨어지는 경우 / 1차원 젠가

--- src/INTERMEDIATE_LOW/Simulation/격자_안에서_터지고_덜어지는_경우/Exercise1.java
+++ src/INTERMEDIATE_LOW/Simulation/격자_안에서_터지고_덜어지는_경우/Exercise1.java
@@ -4,42 +4,46 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//1차원 젠가
 public class Exercise1 {
-    public static int N; //arr size
-    public static int[] arr; //주어지는 배열
+    public static int N; //블록 갯수
+    public static int[] arr;
+    public static int[][] removeSize;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        arr = new int[N+1];
-        for (int i = 1; i < arr.length; i++) {
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
+        removeSize = new int[2][2];
         for (int i = 0; i < 2; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken())-1;
+            int end = Integer.parseInt(st.nextToken())-1;
+            removeSize[i][0] = start;
+            removeSize[i][1] = end;
+        }
 
-            int tempLeng = arr.length - (end - start + 1);
-            int[] temp = new int[tempLeng];
-            int tempIndex = 1;
-
-            for (int index = 1; index < arr.length; index++) {
-                if (index < start || index > end) {
-                    temp[tempIndex++] = arr[index];
+        for (int i = 0; i < 2; i++) {
+            int start = removeSize[i][0];
+            int end = removeSize[i][1];
+            int[] temp = new int[arr.length - (end - start + 1)];
+            int idx = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (j >= start && j <= end) {
+                    continue;
                 }
+                temp[idx++] = arr[j];
             }
             arr = temp;
         }
 
-        StringBuffer sb = new StringBuffer();
-        sb.append(arr.length - 1).append("\n");
-        for (int i = 1; i < arr.length; i++) {
-            sb.append(arr[i]).append("\n");
+        System.out.println(arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
-        System.out.println(sb);
-        br.close();
     }
 }
