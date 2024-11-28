75639ae8220f0d33857baf85afdf7a103459e530
alsry922 <alsry922@gmail.com>
2024-11-29 07:43:19 +0900

    [복습] Backtracking 강력한 폭발

--- src/INTERMEDIATE_LOW/Backtracking/K개_중에_1개를_n번_선택하기_Simple/Exercise3.java
+++ src/INTERMEDIATE_LOW/Backtracking/K개_중에_1개를_n번_선택하기_Simple/Exercise3.java
@@ -8,113 +8,106 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
+
 //강력한 폭발
 public class Exercise3 {
+    public static final int BOMB_TYPE_NUM = 3;
+    public static final int MAX_N = 20;
+
     public static int N; //격자 크기
-    public static int[][] grid;
-    public static int[][] bombed;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    public static int[][] bombed = new int[MAX_N][MAX_N];
+    public static int[][] bombType = new int[MAX_N][MAX_N];
+
     public static List<Pair> bombPos = new ArrayList<>();
-    public static List<Integer> pickBomb = new ArrayList<>();
-    public static int maxArea = 0;
+    public static int answer;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
-        grid = new int[N][N];
-        bombed = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            grid[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
+        StringBuilder sb = new StringBuilder();
 
+        N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j < input.length; j++) {
+                grid[i][j] = input[j];
                 if (grid[i][j] == 1) {
                     bombPos.add(new Pair(i, j));
                 }
             }
         }
 
-        choose(0);
-        System.out.println(maxArea);
-        br.close();
-        bw.close();
+        findMaxArea(bombPos.size());
+
+        System.out.println(answer);
     }
 
-    public static void choose(int n) {
-        if (n == bombPos.size()) {
-            int area = getArea();
-            maxArea = Math.max(area, maxArea);
-            initBombed();
+    public static void findMaxArea(int bombCount) {
+
+        choose(0, bombCount);
+    }
+
+    public static void choose(int pickCnt, int bombCount) {
+        if (pickCnt == bombCount) {
+            int count = simulate();
+            answer = Math.max(answer, count);
             return;
         }
 
         for (int i = 1; i <= 3; i++) {
-            pickBomb.add(i);
-            choose(n + 1);
-            pickBomb.remove(pickBomb.size() - 1);
+            int x = bombPos.get(pickCnt).x;
+            int y = bombPos.get(pickCnt).y;
+            bombType[x][y] = i;
+            choose(pickCnt + 1, bombCount);
+            bombType[x][y] = 0;
         }
     }
 
-    public static void initBombed() {
+    public static int simulate() {
+        // 폭탄이 터진 위치 초기화
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 bombed[i][j] = 0;
             }
         }
-    }
 
-    public static int getArea() {
-        for (int i = 0; i < bombPos.size(); i++) {
-            int bombType = pickBomb.get(i);
-            Pair pos = bombPos.get(i);
-            simulate(bombType, pos.x, pos.y);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (bombType[i][j] != 0) {
+                    bomb(bombType[i][j], i, j);
+                }
+            }
         }
-
-        int area = 0;
+        int count = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (bombed[i][j] == 1) {
-                    area += 1;
+                    count++;
                 }
             }
         }
-
-        return area;
+        return count;
     }
 
-    public static void simulate(int bombType, int x, int y) {
-        if (bombType == 1) {
-            Pair[] area = {
-                    new Pair(-2, 0), new Pair(-1, 0), new Pair(0, 0), new Pair(1, 0), new Pair(2, 0)};
-            for (int i = 0; i < area.length; i++) {
-                int nx = x + area[i].x;
-                int ny = y + area[i].y;
-                if (isInRange(nx, ny)) {
-                    bombed[nx][ny] = 1;
-                }
-            }
-        } else if (bombType == 2) {
-            Pair[] area = {
-                    new Pair(-1, 0), new Pair(0, 1), new Pair(0, 0), new Pair(1, 0), new Pair(0, -1)
-            };
-            for (int i = 0; i < area.length; i++) {
-                int nx = x + area[i].x;
-                int ny = y + area[i].y;
-                if (isInRange(nx, ny)) {
-                    bombed[nx][ny] = 1;
-                }
-            }
-        } else {
-            Pair[] area = {
-                    new Pair(-1, -1), new Pair(-1, 1), new Pair(0, 0), new Pair(1, 1), new Pair(1, -1)
-            };
-            for (int i = 0; i < area.length; i++) {
-                int nx = x + area[i].x;
-                int ny = y + area[i].y;
-                if (isInRange(nx, ny)) {
-                    bombed[nx][ny] = 1;
-                }
+    public static void bomb(int bombType, int x, int y) {
+        Pair[][] bombShapes = {
+                {},
+                {new Pair(-2, 0), new Pair(-1, 0), new Pair(0, 0), new Pair(1, 0), new Pair(2, 0),},
+                {new Pair(-1, 0), new Pair(0, 1), new Pair(0, 0), new Pair(1, 0), new Pair(0, -1),},
+                {new Pair(-1, 1), new Pair(1, 1), new Pair(0, 0), new Pair(1, -1), new Pair(-1, -1),}
+        };
+
+        Pair[] bombAffect = bombShapes[bombType];
+        for (int i = 0; i < bombAffect.length; i++) {
+            int dx = bombAffect[i].x;
+            int dy = bombAffect[i].y;
+
+            int nx = x + dx;
+            int ny = y + dy;
+
+            if (isInRange(nx, ny)) {
+                bombed[nx][ny] = 1;
             }
         }
     }
@@ -126,7 +119,8 @@ public class Exercise3 {
 
 class Pair {
     int x, y;
-    Pair (int x, int y) {
+
+    Pair(int x, int y) {
         this.x = x;
         this.y = y;
     }
