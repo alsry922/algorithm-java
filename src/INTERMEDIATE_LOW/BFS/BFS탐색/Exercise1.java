80e0190fcbccd2a2a5d4f4e833bcb889c14f22f1
alsry922 <alsry922@gmail.com>
2024-12-03 07:35:38 +0900

    [BFS] 네 방향 탈출 가능 여부 판별하기 풀이

--- src/INTERMEDIATE_LOW/BFS/BFS탐색/Exercise1.java
+++ src/INTERMEDIATE_LOW/BFS/BFS탐색/Exercise1.java
@@ -1,8 +1,11 @@
 package INTERMEDIATE_LOW.BFS.BFS탐색;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -23,46 +26,48 @@ public class Exercise1 {
     public static Queue<Pair> bfsQ = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         grid = new int[N][M];
         visited = new boolean[N][M];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                grid[i][j] = Integer.parseInt(st.nextToken());
-            }
+            grid[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
         visited[0][0] = true;
         bfsQ.add(new Pair(0, 0));
 
-        while (!bfsQ.isEmpty()) {
-            Pair currV = bfsQ.poll();
-            int x = currV.x;
-            int y = currV.y;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
 
-            int[] dx = {-1, 1, 0, 0};
-            int[] dy = {0, 0, -1, 1};
+        while(!bfsQ.isEmpty()) {
+            Pair p = bfsQ.poll();
+            int x = p.x, y = p.y;
 
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (canGo(nx, ny)) {
+                if (canMove(nx, ny)) {
                     visited[nx][ny] = true;
                     bfsQ.add(new Pair(nx, ny));
                 }
             }
         }
-        System.out.println(visited[N - 1][M - 1] ? 1 : 0);
+        bw.write(visited[N - 1][M - 1] ? "1" : "0");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static boolean canGo(int x, int y) {
-        return isInRange(x, y) && !visited[x][y] && grid[x][y] == 1;
+    public static boolean canMove(int x, int y) {
+        if (!inRange(x, y)) return false;
+
+        return grid[x][y] == 1 && !visited[x][y];
     }
 
-    private static boolean isInRange(int x, int y) {
+    public static boolean inRange(int x, int y) {
         return x >= 0 && x < N && y >= 0 && y < M;
     }
 }
