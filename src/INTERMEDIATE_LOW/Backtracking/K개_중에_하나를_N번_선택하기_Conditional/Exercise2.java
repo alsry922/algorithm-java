0e71136ef6ec4b3017935432b91be551926320d3
alsry922 <alsry922@gmail.com>
2024-12-09 21:42:48 +0900

    [Backtracking] 1차원 윷놀이

--- src/INTERMEDIATE_LOW/Backtracking/K개_중에_하나를_N번_선택하기_Conditional/Exercise2.java
+++ src/INTERMEDIATE_LOW/Backtracking/K개_중에_하나를_N번_선택하기_Conditional/Exercise2.java
@@ -1,65 +1,64 @@
 package INTERMEDIATE_LOW.Backtracking.K개_중에_하나를_N번_선택하기_Conditional;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
+
 // 1차원 윷놀이
 public class Exercise2 {
-    public static int N, M, K; //턴 수, 윷 판 길이, 말 수
-    public static int[] turns;
-    public static int[] horses;
-    public static ArrayList<Integer> select = new ArrayList<>();
+    public static int N, M, K; //턴 수, 윳놀이 판, 말 수
+    public static int[] distances;
+    public static int[] scores;
+    public static List<Integer> selections = new ArrayList<>();
     public static int answer = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        turns = new int[N];
-        horses = new int[K + 1];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            turns[i] = Integer.parseInt(st.nextToken());
-        }
-        for (int i = 1; i <= K; i++) {
-            horses[i] = 1;
+        distances = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        scores = new int[K + 1];
+        for (int i = 0; i < K + 1; i++) {
+            scores[i] = 1;
         }
 
         choose(0);
-        System.out.println(answer);
+
+        bw.write(answer + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static void choose(int cnt) {
-        if (cnt == N) {
-            initHorses();
-            for (int i = 0; i < select.size(); i++) {
-                horses[select.get(i)] += turns[i];
-            }
-            int score = 0;
-            for (int i = 1; i < horses.length; i++) {
-                if (horses[i] >= M) {
-                    score++;
-                }
-            }
-            answer = Math.max(answer, score);
+    public static void choose(int n) {
+        calc();
+
+        if (n == N) {
             return;
         }
 
         for (int i = 1; i <= K; i++) {
-            select.add(i);
-            choose(cnt + 1);
-            select.remove(select.size() - 1);
+            if (scores[i] >= M)
+                continue;
+
+            scores[i] += distances[n];
+            choose(n + 1);
+            scores[i] -= distances[n];
+
         }
     }
 
-    private static void initHorses() {
-        for (int i = 1; i < horses.length; i++) {
-            horses[i] = 1;
-        }
+    public static void calc() {
+        int result = (int) Arrays.stream(scores).filter(score -> score >= M).count();
+        answer = Math.max(answer, result);
     }
 }
