0fce1377ecbdcde2e9073e0bed938de21ae89cb9
alsry922 <alsry922@gmail.com>
2024-12-11 07:31:29 +0900

    [Backtracking] N개 중에 M개 고르기 / xor 결과 최대 만들기

--- src/INTERMEDIATE_LOW/Backtracking/N개_중에_M개_고르기/Exercise2.java
+++ src/INTERMEDIATE_LOW/Backtracking/N개_중에_M개_고르기/Exercise2.java
@@ -1,54 +1,55 @@
 package INTERMEDIATE_LOW.Backtracking.N개_중에_M개_고르기;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 // xor 결과 최대 만들기
 public class Exercise2 {
-    public static int N, M; //N개 중 M개 고르기
+    public static int N, M; // 숫자 갯수, 뽑을 갯수
     public static int[] nums;
-    public static ArrayList<Integer> select = new ArrayList<>();
+    public static List<Integer> pick = new ArrayList<>();
     public static int answer = Integer.MIN_VALUE;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        nums = new int[N];
+        nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-
-        findMaxXor(0, 0);
-        System.out.println(answer);
+        simulation(N, M);
+        bw.write(answer + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static void findMaxXor(int idx, int cnt) {
-        if (idx == N) {
-            if (cnt == M) {
-                answer = Math.max(answer, calc());
+    public static void simulation(int N, int M) {
+        findCombination(0, 0);
+    }
+
+    public static void findCombination(int n, int m) {
+        if (n == N) {
+            if (m == M) {
+                int result = 0;
+                for (int num : pick) {
+                    result ^= num;
+                }
+                answer = Math.max(answer, result);
             }
             return;
         }
 
-        select.add(nums[idx]);
-        findMaxXor(idx + 1, cnt + 1);
-        select.remove(select.size() - 1);
-        findMaxXor(idx + 1, cnt);
-
-    }
-
-    private static int calc() {
-        int result = select.get(0);
-        for (int i = 1; i < select.size(); i++) {
-            result = result ^ select.get(i);
-        }
-        return result;
+        pick.add(nums[n]);
+        findCombination(n + 1, m + 1);
+        pick.remove(pick.size() - 1);
+        findCombination(n + 1, m);
     }
 }
