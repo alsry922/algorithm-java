5c152e7f8884ba96a4e5f8449341856d200e1de3
alsry922 <alsry922@gmail.com>
2024-12-12 07:53:25 +0900

    [Backtracking] 순열만들기 / 크기가 n인 순열

--- src/INTERMEDIATE_LOW/Backtracking/순열만들기/Exercise1.java
+++ src/INTERMEDIATE_LOW/Backtracking/순열만들기/Exercise1.java
@@ -1,41 +1,45 @@
 package INTERMEDIATE_LOW.Backtracking.순열만들기;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 //크기가 n인 순열
 public class Exercise1 {
     public static int N; //가능한 수 범위
     public static boolean[] selected;
-    public static ArrayList<Integer> permutation = new ArrayList<>();
+    public static List<Integer> permutation = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         selected = new boolean[N + 1];
-        findPermutation(1);
+        findPermutation(0);
     }
 
-    private static void findPermutation(int currDigit) {
-        if (currDigit == N + 1) {
+    public static void findPermutation(int currIdx) {
+        if (currIdx == N) {
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < permutation.size(); i++) {
-                sb.append(permutation.get(i)).append(" ");
+            for (int i : permutation) {
+                sb.append(i).append(" ");
             }
             System.out.println(sb);
             return;
         }
 
         for (int i = 1; i <= N; i++) {
-            if (!selected[i]) {
-                selected[i] = true;
-                permutation.add(i);
-                findPermutation(currDigit + 1);
-                selected[i] = false;
-                permutation.remove(permutation.size() - 1);
+            if (selected[i]) {
+                continue;
             }
+            selected[i] = true;
+            permutation.add(i);
+            findPermutation(currIdx + 1);
+            selected[i] = false;
+            permutation.remove(permutation.size() - 1);
         }
     }
 }
