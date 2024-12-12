5c32854bb6f8555760495e76f4ccdebfa2d0cd4a
alsry922 <alsry922@gmail.com>
2024-12-13 07:43:02 +0900

    [DP1] subproblem을 그대로 합치면 되는 DP / 피보나치 수 풀이(Tabulation)

--- src/INTERMEDIATE_LOW/DP1/subproblem을_그대로_합치면_되는_DP/Exercise1.java
+++ src/INTERMEDIATE_LOW/DP1/subproblem을_그대로_합치면_되는_DP/Exercise1.java
@@ -2,27 +2,28 @@ package INTERMEDIATE_LOW.DP1.subproblem을_그대로_합치면_되는_DP;
 // 피보나치 수
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Exercise1 {
     public static int N;
+    public static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
-
-
-        if (N <= 2) {
-            System.out.println(1);
-        } else {
-            dp[1] = 1;
-            dp[2] = 1;
-            for (int i = 3; i <= N; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            }
-            System.out.println(dp[N]);
+        dp = new int[N + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-
+        bw.write(dp[N] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
