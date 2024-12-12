227ef20d9b2361b67bb64ce0fa69676f82046ff9
alsry922 <alsry922@gmail.com>
2024-12-12 20:38:17 +0900

    [Backtracking] 순열만들기 / 거꾸로 순열 풀이

--- src/INTERMEDIATE_LOW/Backtracking/순열만들기/Exercise2.java
+++ src/INTERMEDIATE_LOW/Backtracking/순열만들기/Exercise2.java
@@ -1,41 +1,49 @@
 package INTERMEDIATE_LOW.Backtracking.순열만들기;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.List;
 // 거꾸로 순열
 
 public class Exercise2 {
-    public static int N; //가능한 수의 범위
-    public static boolean[] selected;
-    public static ArrayList<Integer> permutation = new ArrayList<>();
+    public static int N;
+    public static boolean[] visited;
+    public static List<Integer> pick = new ArrayList<>();
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        selected = new boolean[N + 1];
-
-        getPermutation(0);
+        visited = new boolean[N + 1];
+        findPermutation(0);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static void getPermutation(int cnt) {
-        if (cnt == N) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < permutation.size(); i++) {
-                sb.append(permutation.get(i)).append(" ");
+    public static void findPermutation(int n) {
+        if (n == N) {
+            for (int i : pick) {
+                sb.append(i).append(" ");
             }
-            System.out.println(sb);
+            sb.append("\n");
             return;
         }
 
-        for (int i = N; i >= 1; i--) {
-            if (!selected[i]) {
-                selected[i] = true;
-                permutation.add(i);
-                getPermutation(cnt + 1);
-                selected[i] = false;
-                permutation.remove(permutation.size() - 1);
+        for (int i = N; i > 0; i--) {
+            if (visited[i]) {
+                continue;
             }
+            visited[i] = true;
+            pick.add(i);
+            findPermutation(n + 1);
+            visited[i] = false;
+            pick.remove(pick.size() - 1);
         }
     }
 }
