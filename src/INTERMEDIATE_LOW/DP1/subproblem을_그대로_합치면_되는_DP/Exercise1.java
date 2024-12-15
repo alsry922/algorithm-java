ea785eb78709937e6c04472c24c8d37d9a7506e0
alsry922 <alsry922@gmail.com>
2024-12-15 15:08:36 +0900

    [DP1] subproblem을 그대로 합치면 되는 DP / 피보나치 풀이(memoization)

--- src/INTERMEDIATE_LOW/DP1/subproblem을_그대로_합치면_되는_DP/Exercise1.java
+++ src/INTERMEDIATE_LOW/DP1/subproblem을_그대로_합치면_되는_DP/Exercise1.java
@@ -9,21 +9,40 @@ import java.io.OutputStreamWriter;
 
 public class Exercise1 {
     public static int N;
-    public static int[] dp;
+    public static int[] memo;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-        dp = new int[N + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-        for (int i = 3; i <= N; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        bw.write(dp[N] + "\n");
+        memo = new int[N + 1];
+
+        initMemo();
+        fibo(N);
+
+        bw.write(memo[N] + "\n");
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    public static void initMemo() {
+        for (int i = 0; i <= N; i++) {
+            memo[i] = -1;
+        }
+    }
+
+    public static int fibo(int n) {
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return memo[n] = 1;
+        }
+        memo[n] = fibo(n - 1) + fibo(n - 2);
+
+        return memo[n];
     }
 }
