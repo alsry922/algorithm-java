cd97e5bad8348466a457f6816c01278f229f7da5
alsry922 <alsry922@gmail.com>
2024-12-17 07:47:29 +0900

    [DP1] subproblem을 그대로 합치면 되는 DP / 계단오르기

--- src/INTERMEDIATE_LOW/DP1/subproblem을_그대로_합치면_되는_DP/Exercise2.java
+++ src/INTERMEDIATE_LOW/DP1/subproblem을_그대로_합치면_되는_DP/Exercise2.java
@@ -1,24 +1,33 @@
 package INTERMEDIATE_LOW.DP1.subproblem을_그대로_합치면_되는_DP;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
 
+// 계단오르기
 public class Exercise2 {
-  static final int MAX_N = 1000;    // 최대 계단 높이
-  static int n;
-  static int[] dp = new int[MAX_N+1];
+  public static int N;
+  public static int[] dp;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    n = Integer.parseInt(br.readLine());
-    // dp[i] = 0번 층에서 i층까지 도달알 수 있는 서로 다른 가짓 수
-    dp[0] = 1;
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    N = Integer.parseInt(br.readLine());
+    dp = new int[N + 1];
     dp[1] = 0;
     dp[2] = 1;
-    dp[3] = 1;
-    for (int i = 4; i <= n; i++) {
-      dp[i] = (dp[i-2] + dp[i-3]) % 10007;
+    for (int i = 3; i <= N; i++) {
+      if (i == 3) {
+        dp[i] = 1;
+        continue;
+      }
+      dp[i] = (dp[i - 2] + dp[i - 3]) % 10007;
     }
-    System.out.println(dp[n]);
+    bw.write(String.valueOf(dp[N] % 10007));
+    bw.flush();
+    bw.close();
+    br.close();
   }
 }
