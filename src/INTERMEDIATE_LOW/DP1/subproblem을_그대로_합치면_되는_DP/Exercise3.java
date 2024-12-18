5b9247b6a2532b253bbdee2b981c745182dd457d
alsry922 <alsry922@gmail.com>
2024-12-19 07:42:41 +0900

    [DP1] subproblem을 그대로 합치면 되는 DP / 사각형 채우기

--- src/INTERMEDIATE_LOW/DP1/subproblem을_그대로_합치면_되는_DP/Exercise3.java
+++ src/INTERMEDIATE_LOW/DP1/subproblem을_그대로_합치면_되는_DP/Exercise3.java
@@ -1,22 +1,52 @@
 package INTERMEDIATE_LOW.DP1.subproblem을_그대로_합치면_되는_DP;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+// 사각형 채우기
 public class Exercise3 {
-  static final int MAX_N = 1000;
-  static int n;
-  static int[] dp = new int[MAX_N+1];
+  public static int N;
+  public static int[] dp;
+  public static int[] memo;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    n = Integer.parseInt(br.readLine());
-    /*dp[i] = n이 i 일때 2x0 크기부터 2xi 크기의 격자를 채우는 서로 다른 경우의 수*/
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    N = Integer.parseInt(br.readLine());
+    dp = new int[N + 1];
+    memo = new int[N + 1];
     dp[0] = 1;
     dp[1] = 1;
-    for (int i = 2; i <= n; i++) {
-      dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+    for (int i = 2; i <= N; i++) {
+      dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
     }
-    System.out.println(dp[n]);
+    fillRect(N);
+    bw.write(dp[N] + "\n");
+    bw.write(memo[N] + "\n");
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+
+  public static int fillRect(int n) {
+    if (memo[n] != 0) {
+      return memo[n];
+    }
+
+    if (n == 0) {
+      memo[n] = 1;
+      return memo[n];
+    }
+
+    if (n == 1) {
+      memo[n] = 1;
+      return memo[n];
+    }
+
+    memo[n] = (fillRect(n - 1) + fillRect(n - 2)) % 10007;
+
+    return memo[n];
   }
 }
